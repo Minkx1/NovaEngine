0d536ebff6b90ca4bef6e1c1a0c9d08b77e14048
Minkx1 <zakalata.mark@gmail.com>
2025-08-18 21:34:40 +0300

    PyGameEngine V1.4.0 : Massive Code Reorganization, Custom Player Movements and others.

--- PyGameEngine/__init__.py
+++ PyGameEngine/__init__.py
@@ -3,5 +3,5 @@ from .sprite import Sprite
 from .player import Player
 from .button import Button
 from .scenes import Scene
-from .utils import Colors, render_text, fill_background
-from .movements import PlatformerMovement, TopDownMovement
+from .utils import *
+from .movements import *
